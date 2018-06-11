31b1794928d56f1ae77dd3b77191b3be2a8e0c1f
Laura Jara Yuste <laurajara.yuste@hotmail.com>
2018-06-11 21:48:25 +0200

    actualizando program

--- EjercicioRep/EjercicioRep/Program.cs
+++ EjercicioRep/EjercicioRep/Program.cs
@@ -10,7 +10,13 @@ namespace EjercicioRep
     {
         static void Main(string[] args)
         {
+            Clase1 c1 = new Clase1();
+            Clase2 c2 = new Clase2();
 
+            Console.WriteLine(c1.Suma(3, 2);
+            Console.WriteLine(c1.Resta(4, 2);
+            Console.WriteLine(c2.Multiplicacion(3, 2);
+            Console.WriteLine(c2.Division(10, 2);
         }
     }
 }
